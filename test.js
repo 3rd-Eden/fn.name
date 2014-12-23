9752de3c80d05402aab983a589b5538aa4fa538d
Arnout Kazemier <info@3rd-Eden.com>
2014-12-23 13:48:39 +0100

    [test] Adding some tests to see if the names are correctly extracted.

---
+++ test.js
@@ -0,0 +1,41 @@
+describe('fn.name', function () {
+  'use strict';
+
+  var assume = require('assume')
+    , name = require('./');
+
+  it('is exported as a function', function () {
+    assume(name).is.a('function');
+  });
+
+  it('can extract the name from a function declaration', function () {
+    function foobar() {}
+
+    assume(name(foobar)).equals('foobar');
+  });
+
+  it('can extract the name from a function expression', function () {
+    var a = function bar() {};
+
+    assume(name(a)).equals('bar');
+  });
+
+  it('can be overriden using displayName', function () {
+    var a = function bar() {};
+    a.displayName = 'bro';
+
+    assume(name(a)).equals('bro');
+  });
+
+  it('works with constructed instances', function () {
+    function Bar(){}
+
+    var foo = new Bar();
+
+    assume(name(foo)).equals('Bar');
+  });
+
+  it('works with anonymous', function () {
+    assume(name(function () {})).equals('anonymous');
+  });
+});
